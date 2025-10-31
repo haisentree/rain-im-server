012a4142fd694c221e7ee91511ae1c5dca24e811
haisentree <haisentree@gmail.com>
2025-10-31 18:48:16 +0800

    feat: cmd

--- cmd/tooler/main.go
+++ cmd/tooler/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	toolerCmd "rain-im-server/internal/tooler/cmd"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "tooler",
+	Short: "A brief description of your application",
+	Long:  `etcd get --key "key1" -a 8.148.84.185:2379`,
+}
+
+func init() {
+	rootCmd.AddCommand(toolerCmd.EtcdCmd())
+}
 
 func main() {
-	fmt.Println("test")
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(-1)
+	}
 }
---
+++ internal/tooler/cmd/etcd.go
@@ -0,0 +1,60 @@
+package tooler
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/spf13/cobra"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+func EtcdCmd() *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:   "etcd",
+		Short: "use for etcd config",
+	}
+
+	cmd.AddCommand(etcdGetCmd())
+
+	return cmd
+}
+
+func etcdGetCmd() *cobra.Command {
+	var key string
+	var address string
+
+	cmd := &cobra.Command{
+		Use:   "get",
+		Short: "Get value from etcd",
+		Run: func(cmd *cobra.Command, args []string) {
+			cli, err := clientv3.New(clientv3.Config{
+				Endpoints:   []string{address}, // etcd 节点地址
+				DialTimeout: 5 * time.Second,   // 连接超时时间
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer cli.Close()
+
+			ctx := context.Background()
+
+			resp, err := cli.Get(ctx, key)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, ev := range resp.Kvs {
+				fmt.Printf("Key: %s, Value: %s\n", ev.Key, ev.Value)
+			}
+		},
+	}
+
+	cmd.Flags().StringVarP(&key, "key", "k", "", "Key to get from etcd")
+	cmd.Flags().StringVarP(&address, "address", "a", "", "address is etcd-server address, example:")
+	cmd.MarkFlagRequired("key")
+	cmd.MarkFlagRequired("address")
+
+	return cmd
+}
