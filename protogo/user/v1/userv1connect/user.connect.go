f2187dba0c3f01d0fb09608fbe97644577fe8f4c
haisentree <haisentree@gmail.com>
2025-11-03 18:24:37 +0800

    feat: buf

--- protogo/user/v1/userv1connect/user.connect.go
+++ protogo/user/v1/userv1connect/user.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "github.com/haisentree/rain-im-server/proto/gen/user/v1"
+	v1 "github.com/haisentree/rain-im-server/protogo/user/v1"
 	http "net/http"
 	strings "strings"
 )
