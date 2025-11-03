0c36ce8f0a404af37661621701c77e879a3ddff9
haisentree <haisentree@gmail.com>
2025-11-03 10:53:01 +0800

    feat: 完成etcd基础命令

--- internal/tooler/cmd/etcd.go
+++ internal/tooler/cmd/etcd.go
@@ -18,13 +18,14 @@ func EtcdCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(etcdGetCmd())
+	cmd.AddCommand(etcdGetPrefixCmd())
+	cmd.AddCommand(etcdPutCmd())
 
 	return cmd
 }
 
 func etcdGetCmd() *cobra.Command {
-	var key string
-	var address string
+	var key, address string
 
 	cmd := &cobra.Command{
 		Use:   "get",
@@ -45,6 +46,7 @@ func etcdGetCmd() *cobra.Command {
 			if err != nil {
 				log.Fatal(err)
 			}
+
 			for _, ev := range resp.Kvs {
 				fmt.Printf("Key: %s, Value: %s\n", ev.Key, ev.Value)
 			}
@@ -54,6 +56,78 @@ func etcdGetCmd() *cobra.Command {
 	cmd.Flags().StringVarP(&key, "key", "k", "", "Key to get from etcd")
 	cmd.Flags().StringVarP(&address, "address", "a", "", "address is etcd-server address, example:")
 	cmd.MarkFlagRequired("key")
+	cmd.MarkFlagRequired("address")
+
+	return cmd
+}
+
+func etcdGetPrefixCmd() *cobra.Command {
+	var key, address string
+
+	cmd := &cobra.Command{
+		Use:   "getPrefix",
+		Short: "Get Prefix value from etcd",
+		Run: func(cmd *cobra.Command, args []string) {
+			cli, err := clientv3.New(clientv3.Config{
+				Endpoints:   []string{address}, // etcd 节点地址
+				DialTimeout: 5 * time.Second,   // 连接超时时间
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer cli.Close()
+
+			ctx := context.Background()
+
+			resp, err := cli.Get(ctx, key, clientv3.WithPrefix())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, ev := range resp.Kvs {
+				fmt.Printf("Key: %s, Value: %s\n", ev.Key, ev.Value)
+			}
+		},
+	}
+
+	cmd.Flags().StringVarP(&key, "key", "k", "", "Key to get from etcd")
+	cmd.Flags().StringVarP(&address, "address", "a", "", "address is etcd-server address, example:")
+	cmd.MarkFlagRequired("key")
+	cmd.MarkFlagRequired("address")
+
+	return cmd
+}
+
+func etcdPutCmd() *cobra.Command {
+	var key, value, address string
+
+	cmd := &cobra.Command{
+		Use:   "put",
+		Short: "Put key-value pair to etcd",
+		Run: func(cmd *cobra.Command, args []string) {
+			cli, err := clientv3.New(clientv3.Config{
+				Endpoints:   []string{address}, // etcd 节点地址
+				DialTimeout: 5 * time.Second,   // 连接超时时间
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer cli.Close()
+
+			ctx := context.Background()
+
+			_, err = cli.Put(ctx, key, value)
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+
+	cmd.Flags().StringVarP(&key, "key", "k", "", "Key to put")
+	cmd.Flags().StringVarP(&value, "value", "v", "", "Value to put")
+	cmd.Flags().StringVarP(&address, "address", "a", "", "address is etcd-server address, example:")
+	cmd.MarkFlagRequired("key")
+	cmd.MarkFlagRequired("value")
 	cmd.MarkFlagRequired("address")
 
 	return cmd
